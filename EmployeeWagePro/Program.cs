a2e87b60147f1536ad6089d034565b9e2fcc91dd
rameshwarkale <rbkale125@gmail.com>
2023-02-17 20:49:34 +0530

    intial commit

---
+++ EmployeeWagePro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWagePro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmlopyeeWage computation problem");
+        }
+    }
+}
